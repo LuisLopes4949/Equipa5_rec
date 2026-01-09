427cd7f1b003e8f9c57e53a9bcdf80697945927f
LuisLopes4949 <50362@alunos.upt.pt>
2026-01-09 13:34:46 +0000

    Configuração da ligação à base de dados MySQL com Hibernate

---
+++ despesaspessoais/src/main/java/com/upt/lp/despesaspessoais/DespesasPessoaisApplication.java
@@ -0,0 +1,13 @@
+package com.upt.lp.despesaspessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasPessoaisApplication.class, args);
+	}
+
+}
---
+++ despesaspessoais/src/test/java/com/upt/lp/despesaspessoais/DespesasPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upt.lp.despesaspessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesasPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
